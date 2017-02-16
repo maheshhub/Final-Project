132be6487c4b1c213c805df3548d745bde15c315
Mahesh Gottam <mahesh.gottom@gmail.com>
2017-02-15 21:32:25 -0500

    Master's Final Project
    Twitter Analytics

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+twitter: {
+consumer_key : 'uQheiLKCNl27wHsQiwBnFpRUH',
+consumer_secret : 'F1H0TdqvFMsXAcTefWbfWIFM1aLwghNp7qKsYfopqi2WJAmKyi',
+token : '162070434-UTSE0SPdtuAmRQt8lv5HRvpzRY1u1OjtGxYQ8wdC',
+token_secret : 'oScSIchneAbVqy8geVhhkwCW6mT1FDqsYZSWIC4kmw4n4'
+},
+hana: {
+host : 'trinity.cob.csuchico.edu',
+port : 30059,
+user : 'GBI_361',
+password : 'cob123',
+hdb_schema : 'GBI_361'
+},
+};
+module.exports = config;
