db73c64afab568068a270950adc8cf0def214e20
Mahesh <mahesh.gottom@gmail.com>
2017-02-15 21:36:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ token : '162070434-UTSE0SPdtuAmRQt8lv5HRvpzRY1u1OjtGxYQ8wdC',
 token_secret : 'oScSIchneAbVqy8geVhhkwCW6mT1FDqsYZSWIC4kmw4n4'
 },
 hana: {
-host : 'trinity.cob.csuchico.edu',
-port : 30059,
-user : 'GBI_361',
-password : 'cob123',
-hdb_schema : 'GBI_361'
+host : '********',
+port : ******,
+user : '******',
+password : '*******',
+hdb_schema : '*******'
 },
 };
 module.exports = config;
